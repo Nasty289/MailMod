232e7a2a4935003f8255931d56e6c42f1b87a5a6
Nasty289 <Nasty.289@web.de>
2025-09-07 17:20:59 +0200

    Initial Commit

--- src/main/java/net/nasty289/mailmod/MailMod.java
+++ src/main/java/net/nasty289/mailmod/MailMod.java
@@ -2,6 +2,7 @@ package net.nasty289.mailmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.nasty289.mailmod.block.ModBlocks;
 import net.nasty289.mailmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class MailMod implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/nasty289/mailmod/item/ModItems.java
+++ src/main/java/net/nasty289/mailmod/item/ModItems.java
@@ -10,8 +10,6 @@ import net.nasty289.mailmod.MailMod;
 
 public class ModItems {
 
-    public static final Item MAILBOX = registerItem("mailbox", new Item(new Item.Settings()));
-
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MailMod.MOD_ID, name), item);
     }
@@ -19,8 +17,5 @@ public class ModItems {
     public static void registerModItems() {
         MailMod.LOGGER.info("Registering Mod Items for" + MailMod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
-            entries.add(MAILBOX);
-        });
     }
 }
